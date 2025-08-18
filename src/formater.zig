472cd29f4ee401342e8cd722f871e4b102d0e6b3
jay <immaspring2024@gmail.com>
2025-08-18 12:46:29 -0400

    reworked how the format is stored

--- src/formater.zig
+++ src/formater.zig
@@ -4,87 +4,51 @@ const LexerToken = @import("lexer.zig").Token;
 const LexerTokenType = @import("lexer.zig").TokenType;
 const LexerTokenizer = @import("lexer.zig").Tokenizer;
 
-pub const TextType = enum {
+pub const FormaterElementTag = enum {
+    text,
+    header_start,
+    header_end,
+    bold_start,
+    bold_end,
+    italics_start,
+    italics_end,
+    code_start,
+    code_end,
+    code_block_start,
+    code_block_end,
+};
+
+pub const FormaterElement = union(FormaterElementTag) {
+    text: []const u8,
+    header_start: HeaderLevel,
+    header_end: void,
+    bold_start: void,
+    bold_end: void,
+    italics_start: void,
+    italics_end: void,
+    code_start: void,
+    code_end: void,
+    code_block_start: []const u8,
+    code_block_end: void,
+};
+
+pub const HeaderLevel = enum {
     header_1,
     header_2,
     header_3,
     header_4,
     header_5,
     header_6,
-    text,
-};
-
-pub const MarkdownElementTag = enum {
-    text,
-    ordered_list,
-    unordered_list,
-    code_block,
-    link,
-};
-
-pub const MarkdownElement = union(MarkdownElementTag) {
-    text: Text,
-    ordered_list: OrderedListElement,
-    unordered_list: UnorderedListElement,
-    code_block: CodeBlock,
-    link: Link,
-};
-
-pub const MarkdownElementNode = struct {
-    element: MarkdownElement,
-    next_node: ?*MarkdownElementNode = null,
-    prev_node: ?*MarkdownElementNode = null,
-};
-
-pub const Text = struct {
-    type: TextType,
-    content: []const u8,
-    bold: bool,
-    italics: bool,
-    code: bool,
-};
-
-pub const OrderedListElement = struct {
-    item_number: usize = 1,
-    content_start: ?*MarkdownElementNode = null,
-    constent_end: ?*MarkdownElementNode = null,
-};
-
-pub const UnorderedListElement = struct {
-    content_start: ?*MarkdownElementNode = null,
-    content_end: ?*MarkdownElementNode = null,
-};
-
-pub const Link = struct {
-    image: bool = false,
-    tag: []const u8,
-    link_start: ?*MarkdownElementNode = null,
-    link_end: ?*MarkdownElementNode = null,
-};
-
-pub const CodeBlock = struct {
-    type: []const u8 = undefined,
-    content_start: ?*MarkdownElementNode = null,
-    content_end: ?*MarkdownElementNode = null,
-};
-
-pub const Mode = enum {
-    italic,
-    bold,
-    code,
-    list,
 };
 
 pub const TokenFormater = struct {
-    prev_token: LexerToken = .{.type = .eof, .start = 0, .end = 0},
-    current_token: LexerToken = undefined,
-    mode: std.ArrayList(Mode) = undefined,
+    tokens: std.ArrayList(LexerToken),
+    //mode: std.DoublyLinkedList(Mode) = undefined,
 
-    pub fn format(this: *TokenFormater, tokenizer: LexerTokenizer, allocator: std.mem.Allocator) *MarkdownElementNode {
-        this.mode = std.ArrayList(Mode).init(allocator);
-        this.current_token = tokenizer.nextToken();
-        while (this.current_token.type != .eof) {
-            switch () {}
-        }
+    pub fn format(this: *TokenFormater, tokenizer: LexerTokenizer, allocator: std.mem.Allocator) std.DoublyLinkedList(FormaterElement) {
+        this.tokens = tokenizer.getTokens(allocator);
+        const format_tokens: std.DoublyLinkedList(FormaterElement) = std.DoublyLinkedList(FormaterElement);
+
+        return format_tokens;
     }
 };
